102d5a89b25ad9de91da02f6153c013c849a9d54
Alex Carol <acarolcampre@adobe.com>
2025-02-04 09:02:47 +0100

    Add console log to test branch deploy

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+	console.log('columns block');
 
   // setup image columns
   [...block.children].forEach((row) => {
